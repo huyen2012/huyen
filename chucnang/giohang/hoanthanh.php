9f6fd1052c70cba688152fea029f4289ed489ce8
Your Name <you@example.com>
2021-08-05 09:52:58 +0700

    first commit

---
+++ chucnang/giohang/hoanthanh.php
@@ -0,0 +1,11 @@
+<div id="wrap-inner">
+    <div id="complete">
+        <p class="info">Quý khách đã đặt hàng thành công!</p>
+        <p>• Sản phẩm của Quý khách sẽ được chuyển đến địa chỉ có trong phần thông tin khách hàng của chúng tôi sau thời
+            gian 3 đến 4 ngày, tính từ thời điểm này.</p>
+        <p>• Nhân viên giao hàng sẽ liên hệ với quý khách qua số điện thoại trước khi giao hàng 24 tiếng</p>
+        <p>• Trụ sở chính: Hoàng Văn Thái - Thanh Xuân - Hà Nội</p>
+        <p>Cám ơn quý khách đã sử dụng sản phẩm của cửa hàng chúng tôi!</p>
+    </div>
+    <p class="text-right return"><a href="index.php">Quay lại trang chủ</a></p>
+</div>
